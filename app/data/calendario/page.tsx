cab59f6d145a5d6de2bcb2b6dc0c88dc268da766
Orozco Ceron <E.Orozco@cgiar.org>
2024-09-03 15:46:04 -0500

    update calendar

--- app/data/calendario/page.tsx
+++ app/data/calendario/page.tsx
@@ -1,35 +1,14 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import dayjs from "dayjs";
-import { Calendar, dayjsLocalizer } from "react-big-calendar";
-import "react-big-calendar/lib/css/react-big-calendar.css";
-import "dayjs/locale/es";
-import updateLocale from "dayjs/plugin/updateLocale";
 import style from "./calendar.module.css";
-import CalendarModal from "@/components/Modals/CalendarModal/CalendarModal";
-import MapComponent from "@/components/data/Map/MapComponent";
 import { EventsData, DataFormat, sectionStateData } from "@/interfaces";
 import CalendarRepository from "@/helpers/Component/Repository/CalendarRepository";
 import CalendarController from "@/helpers/Component/Controller/CalendarController";
-import { Card, Container } from "@mui/material";
 import ChartCardComponent from "@/components/events/chartCard";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid"
 
-
-dayjs.extend(updateLocale);
-dayjs.locale("es");
-dayjs.updateLocale("es", {
-  weekdays: [
-    "Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"
-  ],
-  months: [
-    "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"
-  ]
-});
-
-const localizer = dayjsLocalizer(dayjs);
 
 export default function DataCalendarResults() {
   const [events, setEvents] = useState<EventsData[]>([]);
@@ -81,34 +60,8 @@ export default function DataCalendarResults() {
     setFilteredEvents(tempEvens);
   };
 
-  const handleSelectedEvent = (event: EventsData) => {
-    setSelectedEvent(event);
-    setModalIsOpen(true);
-  };
 
-  const closeModal = () => {
-    setModalIsOpen(false);
-    setSelectedEvent(null);
-  };
 
-  const messages = {
-    date: "Fecha",
-    time: "Hora",
-    event: "Evento",
-    allDay: "Todo el día",
-    week: "Semana",
-    work_week: "Semana laboral",
-    day: "Día",
-    month: "Mes",
-    previous: "Anterior",
-    next: "Siguiente",
-    yesterday: "Ayer",
-    tomorrow: "Mañana",
-    today: "Hoy",
-    agenda: "Agenda",
-    noEventsInRange: "No hay eventos en este rango",
-    showMore: (total: number) => `+ Ver más (${total})`
-  };
 
   return (
     <ChartCardComponent title="esto" header={<></>}>
