78de84e6e22eb2988a11e7910bef22d4174caabe
Orozco Ceron <E.Orozco@cgiar.org>
2024-09-07 17:37:46 -0500

    update event calendar url

--- helpers/Component/Repository/CalendarRepository.ts
+++ helpers/Component/Repository/CalendarRepository.ts
@@ -1,8 +1,8 @@
-import {DataFormat} from "@/interfaces";
+import { DataFormat } from "@/interfaces";
 
 class CalendarRepository {
     static async fetchEvents(): Promise<DataFormat> {
-        const response = await fetch("https://cxe00413s7.execute-api.us-east-1.amazonaws.com/get-events");
+        const response = await fetch("https://qhl00jvv1b.execute-api.us-east-1.amazonaws.com/dev/get-events");
         if (!response.ok) {
             throw new Error("Failed to fetch events");
         }
