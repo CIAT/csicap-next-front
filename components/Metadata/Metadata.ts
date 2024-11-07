a99d978d47b9f99a572c3c494098475307cad48f
lossa <L.Ossa@cgiar.org>
2024-11-07 10:50:26 -0500

    Feat(metadata): image has been fixed. Now is shown.

--- components/Metadata/Metadata.ts
+++ components/Metadata/Metadata.ts
@@ -2,8 +2,8 @@ export const metadata = {
     title: "CSICAP",
     description: "Colombia Agroalimentaria Sostenible: adaptación al cambio climático",
     openGraph: {
-        title: "CSICAP",
-        description: "Colombia Agroalimentaria Sostenible: adaptación al cambio climático",
+        title: "Colombia Agroalimentaria Sostenible",
+        description: "Adaptación al cambio climático",
         image: {
             url: "/metadata_logo.png",
             alt: "CSICAP",
