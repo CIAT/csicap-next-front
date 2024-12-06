bc3f572c9ef7c34e331333d0ea3ea745c7f92f95
sguerrero <s.guerrero@cgair.org>
2024-12-06 09:34:09 -0500

    fixed bugs

--- components/ui/card/Card.tsx
+++ components/ui/card/Card.tsx
@@ -5,23 +5,24 @@ import {
     Divider
 } from "@nextui-org/react";
 import React, {ReactNode} from "react";
-import styles from "../../../app/assistance/assistance.module.css";
+import styles from "@/components/components.module.css";
 import ExportDropdown from "@/components/download/DowloadDropDown/ExportDropdown";
 
 interface CardProps {
+    styles: { [key: string]: string };
     title: string;
     children: ReactNode;
     id?: string;
     data?: string[];
 }
 
-const CardComponent: React.FC<CardProps> = ({title, children, id, data }) => {
+const CardComponent: React.FC<CardProps> = ({ styles, title, children, id, data }) => {
     return (
         <div className={styles.card}>
             <Card className={styles.card_container}>
                 <CardHeader className={styles.card_header}>
                     <div className={styles.header}>
-                        <p className={` ${styles.text_black}`}>{title}</p>
+                        <p className={`${styles.text_black}`}>{title}</p>
                         {(id || data) && (
                             <ExportDropdown
                                 chartId={id}
