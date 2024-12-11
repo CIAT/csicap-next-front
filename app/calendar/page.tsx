3b554f06bcef4e7447d819b562b3de8999c594ed
sguerrero <s.guerrero@cgair.org>
2024-12-11 08:32:12 -0500

    css for tooltip

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -7,7 +7,7 @@ import CalendarRepository from "@/helpers/Component/Repository/CalendarRepositor
 import CalendarController from "@/helpers/Component/Controller/CalendarController";
 import ChartCardComponent from "@/components/events/chartCard";
 import FullCalendar from "@fullcalendar/react";
-import dayGridPlugin from "@fullcalendar/daygrid"
+import dayGridPlugin from "@fullcalendar/daygrid";
 import CalendarModal from "@/components/Modals/CalendarModal/CalendarModal";
 import dayjs from "dayjs";
 import MapComponent from "@/components/data/Map/MapComponent";
@@ -16,26 +16,28 @@ import CardComponent from "@/components/calendar/Card/CardComponent";
 import LoadingAnimation from "@/components/loadingAnimation";
 
 import MapController from "@/helpers/Component/Controller/MapController";
-import {NestedDictionary} from "@/interfaces/Map/NestedDictionary";
-import {parseISO} from "date-fns";
-import {NextPage} from "next";
-import {PageCustomProps} from "@/interfaces/Components/PageCustomProps";
-import {CustomTooltipData} from "@/interfaces/Components/CustomTooltip";
+import { NestedDictionary } from "@/interfaces/Map/NestedDictionary";
+import { parseISO } from "date-fns";
+import { NextPage } from "next";
+import { PageCustomProps } from "@/interfaces/Components/PageCustomProps";
+import { CustomTooltipData } from "@/interfaces/Components/CustomTooltip";
 import EventsController from "@/helpers/Component/Controller/EventsController";
 import CustomTooltip from "@/components/CustomTooltip/CustomTooltip";
-import {handleOnClick, handleReset, handleTooltipChange} from "@/helpers/Component/CustomTooltip/CustomTooltipHandler";
+import {
+  handleOnClick,
+  handleReset,
+  handleTooltipChange,
+} from "@/helpers/Component/CustomTooltip/CustomTooltipHandler";
 import {
   filterFunctionsCalendar,
   getUniqueValuesFunctionsCalendar,
 } from "@/interfaces/Components/CustomTooltipHandler";
 
-const CalendarPage:NextPage<PageCustomProps> = ({customStyles}) => {
+const CalendarPage: NextPage<PageCustomProps> = ({ customStyles }) => {
   const styles = customStyles || require("./calendar.module.css");
 
   const [events, setEvents] = useState<EventsData[]>([]);
-  const [tempEventData, setTempEventData] = useState<EventsData[]>(
-    events
-  );
+  const [tempEventData, setTempEventData] = useState<EventsData[]>(events);
   const [filtersApplied, setFiltersApplied] = useState(false);
 
   const [dataCalendarResp, setDataCalendarResp] = useState<number>(0);
@@ -44,46 +46,80 @@ const CalendarPage:NextPage<PageCustomProps> = ({customStyles}) => {
   const [sectionState, setSectionState] = useState<sectionStateData>({
     axe: "",
     crop: "",
-    city: ""
+    city: "",
   });
   const [componentState, setComponentState] = useState<CustomTooltipData[]>([]);
   const [axisState, setAxisState] = useState<CustomTooltipData[]>([]);
-  const [institutionState, setInstitutionState] = useState<CustomTooltipData[]>([]);
+  const [institutionState, setInstitutionState] = useState<CustomTooltipData[]>(
+    []
+  );
   const [cropState, setCropState] = useState<CustomTooltipData[]>([]);
-  const [departmentState, setDepartmentState] = useState<CustomTooltipData[]>([]);
+  const [departmentState, setDepartmentState] = useState<CustomTooltipData[]>(
+    []
+  );
   const [cityState, setCityState] = useState<CustomTooltipData[]>([]);
   const [counts, setCounts] = useState<NestedDictionary>({});
 
   const [tooltipValues, setTooltipValues] = useState<Array<CustomTooltipData>>([
     {
       value: "",
-      label: "Componente"
+      label: "Componente",
     },
     {
       value: "",
-      label: "Eje"
+      label: "Eje",
     },
     {
       value: "",
-      label: "Institución"
+      label: "Institución",
     },
     {
       value: "",
-      label: "Cultivo"
+      label: "Cultivo",
     },
     {
       value: "",
-      label: "Departamento"
+      label: "Departamento",
     },
     {
       value: "",
-      label: "Municipio"
-    }
-  ])
-  const tooltipOptions: Array<CustomTooltipData[]> = [componentState, axisState, institutionState, cropState, departmentState, cityState];
-  const setTooltipOptions: Array<React.Dispatch<React.SetStateAction<CustomTooltipData[]>>> = [setComponentState, setAxisState, setInstitutionState, setCropState, setDepartmentState, setCityState];
-  const filterTypes = ["component", "axis", "institution", "crop", "department", "city"];
-  const placeHolders = ["Componente", "Eje", "Institución", "Cultivo", "Departamento", "Municipio"];
+      label: "Municipio",
+    },
+  ]);
+  const tooltipOptions: Array<CustomTooltipData[]> = [
+    componentState,
+    axisState,
+    institutionState,
+    cropState,
+    departmentState,
+    cityState,
+  ];
+  const setTooltipOptions: Array<
+    React.Dispatch<React.SetStateAction<CustomTooltipData[]>>
+  > = [
+    setComponentState,
+    setAxisState,
+    setInstitutionState,
+    setCropState,
+    setDepartmentState,
+    setCityState,
+  ];
+  const filterTypes = [
+    "component",
+    "axis",
+    "institution",
+    "crop",
+    "department",
+    "city",
+  ];
+  const placeHolders = [
+    "Componente",
+    "Eje",
+    "Institución",
+    "Cultivo",
+    "Departamento",
+    "Municipio",
+  ];
 
   const handleEventClick = (clickInfo: any) => {
     setSelectedEvent(clickInfo.event.extendedProps);
@@ -100,12 +136,34 @@ const CalendarPage:NextPage<PageCustomProps> = ({customStyles}) => {
       .then((data: DataFormat) => {
         const formattedEvents = CalendarController.formatEvents(data);
 
-        const uniqueAxis = EventsController.getUniqueValues(formattedEvents, "eje", true);
-        const uniqueComponents = EventsController.getUniqueValues(formattedEvents, "component", true);
-        const uniqueInstitutions = EventsController.getUniqueValues(formattedEvents, "institution", true);
-        const uniqueCrops = EventsController.getUniqueValues(formattedEvents, "crop", true);
-        const uniqueDepartments = EventsController.getUniqueValues(formattedEvents, "province");
-        const uniqueCities = EventsController.getUniqueValues(formattedEvents, "city");
+        const uniqueAxis = EventsController.getUniqueValues(
+          formattedEvents,
+          "eje",
+          true
+        );
+        const uniqueComponents = EventsController.getUniqueValues(
+          formattedEvents,
+          "component",
+          true
+        );
+        const uniqueInstitutions = EventsController.getUniqueValues(
+          formattedEvents,
+          "institution",
+          true
+        );
+        const uniqueCrops = EventsController.getUniqueValues(
+          formattedEvents,
+          "crop",
+          true
+        );
+        const uniqueDepartments = EventsController.getUniqueValues(
+          formattedEvents,
+          "province"
+        );
+        const uniqueCities = EventsController.getUniqueValues(
+          formattedEvents,
+          "city"
+        );
 
         setCounts(MapController.updateCountEventsByCity(formattedEvents));
 
@@ -120,7 +178,7 @@ const CalendarPage:NextPage<PageCustomProps> = ({customStyles}) => {
         setEvents(formattedEvents);
         setTempEventData(formattedEvents);
       })
-      .catch(error => {
+      .catch((error) => {
         console.error("Error fetching events:", error);
         setDataCalendarResp(-1);
       });
@@ -134,7 +192,7 @@ const CalendarPage:NextPage<PageCustomProps> = ({customStyles}) => {
     setSectionState({
       axe: "",
       crop: "",
-      city: ""
+      city: "",
     });
     MapController.resetSelectedProvinceAndCity();
     setTempEventData(events);
@@ -144,138 +202,166 @@ const CalendarPage:NextPage<PageCustomProps> = ({customStyles}) => {
   return (
     <div className={styles.container}>
       {dataCalendarResp === 200 ? (
-          <>
-            <CustomTooltip
-                options={tooltipOptions}
-                values={tooltipValues}
-                onChange={(selectedValue, filterType) =>
-                    handleTooltipChange(
-                        selectedValue,
-                        filterType,
-                        tempEventData,
-                        setTooltipOptions,
-                        tooltipValues,
-                        setTooltipValues,
-                        filterFunctionsCalendar,
-                        getUniqueValuesFunctionsCalendar(),
-                        filterTypes
-                    )}
-                onClick={() =>
-                    handleOnClick(
-                        tooltipValues,
-                        tempEventData,
-                        setTempEventData,
-                        filterFunctionsCalendar,
-                        filterTypes
-                    )}
-                onReset={() =>
-                    handleReset(
-                        events,
-                        setTooltipOptions,
-                        setTooltipValues,
-                        setTempEventData,
-                        getUniqueValuesFunctionsCalendar(),
-                        placeHolders
-                    )}
-                placeholders={placeHolders}
-                filterTypes={filterTypes}
-                getOptionLabel={(option) => option.label}
-                getOptionValue={(option) => String(option.value)}
-            />
+        <>
+          <CustomTooltip
+            options={tooltipOptions}
+            values={tooltipValues}
+            onChange={(selectedValue, filterType) =>
+              handleTooltipChange(
+                selectedValue,
+                filterType,
+                tempEventData,
+                setTooltipOptions,
+                tooltipValues,
+                setTooltipValues,
+                filterFunctionsCalendar,
+                getUniqueValuesFunctionsCalendar(),
+                filterTypes
+              )
+            }
+            onClick={() =>
+              handleOnClick(
+                tooltipValues,
+                tempEventData,
+                setTempEventData,
+                filterFunctionsCalendar,
+                filterTypes
+              )
+            }
+            onReset={() =>
+              handleReset(
+                events,
+                setTooltipOptions,
+                setTooltipValues,
+                setTempEventData,
+                getUniqueValuesFunctionsCalendar(),
+                placeHolders
+              )
+            }
+            placeholders={placeHolders}
+            filterTypes={filterTypes}
+            getOptionLabel={(option) => option.label}
+            getOptionValue={(option) => String(option.value)}
+          />
+          <div style={{display:"flex", flexDirection:"row"}}>
             <div className={styles.card_container}>
               <div className={styles.overview}>
-                <CardComponent title="Visión General" header={<></>} style={cardStyle}>
+                <CardComponent
+                  title="Visión General"
+                  header={<></>}
+                  style={cardStyle}
+                >
                   <OverviewCard></OverviewCard>
                 </CardComponent>
               </div>
               <div className={styles.sub_card_container}>
-                <ChartCardComponent title="Calendario de eventos" header={<></>}>
+                <ChartCardComponent
+                  title="Calendario de eventos"
+                  header={<></>}
+                >
                   <FullCalendar
-                      plugins={[dayGridPlugin]}
-                      headerToolbar={{
-                        left: 'prev,next today',
-                        center: 'title',
-                        right: ''
-                      }}
-                      height='100%'
-                      fixedWeekCount={false}
-                      events={tempEventData.map(event => {
+                    plugins={[dayGridPlugin]}
+                    headerToolbar={{
+                      left: "prev,next today",
+                      center: "title",
+                      right: "",
+                    }}
+                    height="100%"
+                    fixedWeekCount={false}
+                    events={tempEventData
+                      .map((event) => {
                         const currentDate = new Date();
                         currentDate.setHours(0, 0, 0, 0);
-                        const eventEndDate = event.datesEnd ? parseISO(String(event.datesEnd)) : null;
+                        const eventEndDate = event.datesEnd
+                          ? parseISO(String(event.datesEnd))
+                          : null;
 
                         let backgroundColor;
                         let borderColor;
 
-                        if(event.change_selection === 'EL EVENTO HA SIDO CANCELADO'){
-                          backgroundColor = '#b9b9b9';
-                          borderColor = '#b9b9b9';
-                        } else if ((eventEndDate && event.form_state === '1' && eventEndDate < currentDate) || event.not_assistant === '1') {
-                          backgroundColor = '#c84e42';
-                          borderColor = '#c84e42';
-                        } else if (event.form_state === '0') {
+                        if (
+                          event.change_selection ===
+                          "EL EVENTO HA SIDO CANCELADO"
+                        ) {
+                          backgroundColor = "#b9b9b9";
+                          borderColor = "#b9b9b9";
+                        } else if (
+                          (eventEndDate &&
+                            event.form_state === "1" &&
+                            eventEndDate < currentDate) ||
+                          event.not_assistant === "1"
+                        ) {
+                          backgroundColor = "#c84e42";
+                          borderColor = "#c84e42";
+                        } else if (event.form_state === "0") {
                           // Eventos que están completamente finalizados (form_state = 0)
-                          backgroundColor = '#80C41C';
-                          borderColor = '#80C41C';
-                        } else if (eventEndDate && eventEndDate >= currentDate) {
+                          backgroundColor = "#80C41C";
+                          borderColor = "#80C41C";
+                        } else if (
+                          eventEndDate &&
+                          eventEndDate >= currentDate
+                        ) {
                           // Si el evento aún no ha terminado o es hoy
-                          backgroundColor = '#FECF00';
-                          borderColor = '#FECF00';
+                          backgroundColor = "#FECF00";
+                          borderColor = "#FECF00";
                         }
 
                         return {
                           ...event,
                           backgroundColor,
-                          borderColor
+                          borderColor,
                         };
-                      }).filter(event => event !== null)}
-                      eventClick={handleEventClick}
+                      })
+                      .filter((event) => event !== null)}
+                    eventClick={handleEventClick}
                   />
                 </ChartCardComponent>
               </div>
             </div>
-            <div className={styles.card_container}>
+            <div className={styles.card_container2}>
               <ChartCardComponent title="Eventos por municipio" header={<></>}>
                 <MapComponent
-                    data={counts}
-                    polygons={CalendarController.extractProvincesAndCities(tempEventData)}
+                  data={counts}
+                  polygons={CalendarController.extractProvincesAndCities(
+                    tempEventData
+                  )}
                 />
               </ChartCardComponent>
             </div>
-          </>
-      ) : dataCalendarResp === 0 ? (
-          <div className={styles.loading_container}>
-            <div className={styles.loading}>
-              <LoadingAnimation/>
-            </div>
           </div>
+        </>
+      ) : dataCalendarResp === 0 ? (
+        <div className={styles.loading_container}>
+          <div className={styles.loading}>
+            <LoadingAnimation />
+          </div>
+        </div>
       ) : (
-          "Ups! something went wrong, try later"
-      )},
-      {
-      selectedEvent && (
-          <CalendarModal
-            title={selectedEvent.name}
-            show={modalIsOpen}
-            handleClose={closeModal}
-            //@ts-ignore
-            //TODO: fix this datexx
-            eventDate={dayjs(selectedEvent.initialDate).format("YYYY-MM-DD")}
-            eventDatend={dayjs(selectedEvent.datesEnd).format("YYYY-MM-DD")}
-            province={selectedEvent.province}
-            axis={selectedEvent.eje}
-            organizer={selectedEvent.responsable || "N/A"}
-            objetive={selectedEvent.event_objective}
-            city={selectedEvent.city}
-            crop={selectedEvent.crop}
-            institution={selectedEvent.institution}
-            guesType={selectedEvent.guess_type}
-            email={selectedEvent.email}
-          />
-        )
-      }
-    </div >
+        "Ups! something went wrong, try later"
+      )}
+      ,
+      {selectedEvent && (
+        <CalendarModal
+          title={selectedEvent.name}
+          show={modalIsOpen}
+          handleClose={closeModal}
+          //@ts-ignore
+          //TODO: fix this datexx
+          eventDate={dayjs(selectedEvent.initialDate).format("YYYY-MM-DD")}
+          eventDatend={dayjs(selectedEvent.datesEnd).format("YYYY-MM-DD")}
+          province={selectedEvent.province}
+          axis={selectedEvent.eje}
+          organizer={selectedEvent.responsable || "N/A"}
+          objetive={selectedEvent.event_objective}
+          city={selectedEvent.city}
+          crop={selectedEvent.crop}
+          institution={selectedEvent.institution}
+          guesType={selectedEvent.guess_type}
+          email={selectedEvent.email}
+        />
+      )}
+    </div>
   );
-}
+};
 
 export default CalendarPage;
--- components/CustomTooltip/CustomTooltip.tsx
+++ components/CustomTooltip/CustomTooltip.tsx
@@ -1,56 +1,63 @@
 import React from "react";
 import Select from "react-select";
 import { Button } from "@mui/material";
+import styles from "./tooltip.module.css";
 
 interface MultiSelectProps<T> {
-    options: Array<Array<T>>;
-    values: Array<T | null>;
-    onChange: (selectedOption: string, filterType: string) => void;
-    onClick: () => void;
-    onReset: () => void;
-    placeholders: Array<string>;
-    filterTypes: Array<string>;
-    getOptionLabel?: (option: T) => string;
-    getOptionValue?: (option: T) => string;
+  options: Array<Array<T>>;
+  values: Array<T | null>;
+  onChange: (selectedOption: string, filterType: string) => void;
+  onClick: () => void;
+  onReset: () => void;
+  placeholders: Array<string>;
+  filterTypes: Array<string>;
+  getOptionLabel?: (option: T) => string;
+  getOptionValue?: (option: T) => string;
 }
 
 const CustomTooltip = <T,>({
-                               options,
-                               values,
-                               onChange,
-                               onClick,
-                               onReset,
-                               placeholders,
-                               filterTypes,
-                               getOptionLabel = (option) => String(option),
-                               getOptionValue = (option) => String(option),
-                           }: MultiSelectProps<T>) => {
-    return (
-        <div>
-            {options.map((selectOptions, index) => (
-                <div key={index} style={{ marginBottom: "1rem" }}>
-                    <Select
-                        options={selectOptions}
-                        value={values[index]}
-                        onChange={(selectedOption) => {
-                            const selectedValue = selectedOption
-                                ? getOptionValue(selectedOption)
-                                : '';
-                            onChange(selectedValue, filterTypes[index]);
-                        }}
-                        getOptionLabel={getOptionLabel}
-                        getOptionValue={getOptionValue}
-                        placeholder={placeholders[index]}
-                        isSearchable
-                    />
-                </div>
-            ))}
-            <div>
-                <Button onClick={onClick}>Aplicar</Button>
-                <Button onClick={onReset}>Restaurar</Button>
-            </div>
-        </div>
-    );
+  options,
+  values,
+  onChange,
+  onClick,
+  onReset,
+  placeholders,
+  filterTypes,
+  getOptionLabel = (option) => String(option),
+  getOptionValue = (option) => String(option),
+}: MultiSelectProps<T>) => {
+  return (
+    <div className={styles.container}>
+      <div style={{ display: "flex", flexDirection: "row", gap: "1vw" }}>
+        {options.map((selectOptions, index) => (
+          <div key={index} style={{}}>
+            <Select
+              options={selectOptions}
+              value={values[index]}
+              onChange={(selectedOption) => {
+                const selectedValue = selectedOption
+                  ? getOptionValue(selectedOption)
+                  : "";
+                onChange(selectedValue, filterTypes[index]);
+              }}
+              getOptionLabel={getOptionLabel}
+              getOptionValue={getOptionValue}
+              placeholder={placeholders[index]}
+              isSearchable
+            />
+          </div>
+        ))}
+      </div>
+      <div>
+        <Button onClick={onClick} className={styles.button}>
+          Aplicar
+        </Button>
+        <Button onClick={onReset} className={styles.button}>
+          Restaurar
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default CustomTooltip;
