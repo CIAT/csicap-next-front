9264662ce044839a261dc1eb19609a1eb28c8641
lossa <L.Ossa@cgiar.org>
2025-01-22 16:21:07 -0500

    Feat(Home): landing has been added

--- app/Home.tsx
+++ app/Home.tsx
@@ -5,8 +5,22 @@ import styles from "./home.module.css";
 export default function Home() {
 
     return (
-        <div>
-            <div className={styles.landing}></div>
+        <div className={styles.container}>
+            <div className={`${styles.spacing} ${styles.landing}`}>
+                <div className={`${styles.spacing} ${styles.text} ${styles.title}`}>
+                    Plataforma de monitoreo, evaluación, aprendizaje y evaluación de impacto del proyecto Colombia Agroalimentaria Sostenible
+                </div>
+                <div className={`${styles.spacing} ${styles.text}`}>
+                    La Plataforma de Monitoreo y Evaluación es una herramienta diseñada para recopilar, gestionar y analizar indicadores clave del proyecto de manera eficiente y segura.
+                    <br/>
+                    Este sistema ofrece tableros interactivos que resumen el avance de los indicadores, proporcionando información en tiempo real para facilitar la toma de decisiones y mejorar la implementación del proyecto.
+                </div>
+                <div>
+                    <button className={`${styles.spacing} ${styles.button}`}>
+                        Conocer más
+                    </button>
+                </div>
+            </div>
         </div>
     );
 }
