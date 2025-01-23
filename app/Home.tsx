868b1a6bfc7842689fdfc9f8629b9f3993b7658a
lossa <L.Ossa@cgiar.org>
2025-01-23 10:46:03 -0500

    Feat(Home): text has been added

--- app/Home.tsx
+++ app/Home.tsx
@@ -8,23 +8,47 @@ export default function Home() {
         if (section) {
             section.scrollIntoView({ behavior: 'smooth' });
         }
-    }
+    };
+
+    // Define los datos de las cards en un arreglo
+    const cards = [
+        {
+            icon: "/chart_icon.png",
+            alt: "chart-icon",
+            text: (
+                <>
+                    El Monitoreo es un proceso continuo de recolección y análisis de información rutinaria sobre indicadores específicos, con el fin de evaluar el progreso de las actividades relacionadas a unas metas específicas. <br />
+                    En esta sección podrás consultar información detallada sobre las actividades del proyecto CSICAP y su avance en tiempo real. Accede a un calendario interactivo que muestra las actividades distribuidas por municipio, cultivo y eje, con la posibilidad de aplicar filtros según tus necesidades. Además, encontrarás estadísticas de los eventos registrados, información sobre los asistentes y reportes consolidados por evento, lo que te permitirá realizar un seguimiento completo del progreso y los beneficiarios en diferentes niveles.
+                </>
+            ),
+            buttonText: "Monitoreo",
+            buttonLink: "/monitoring/calendar",
+        },
+        {
+            icon: "/list_icon.png",
+            alt: "list-icon",
+            text: (
+                <>
+                    La Evaluación determina la relevancia y pertinencia del proyecto por medio del logro de los objetivos que inicialmente se plantearon. De esta manera, se intenta establecer el grado de atribución y causalidad de las intervenciones. <br />
+                    En esta sección podrás consultar la evolución de los indicadores clave del proyecto, que reflejan el impacto en áreas como la adopción de prácticas agroambientales, el fortalecimiento de capacidades, la mejora en la productividad de los cultivos, y la sostenibilidad económica y ambiental. A través de visualizaciones claras, podrás seguir el progreso de estos objetivos y el impacto en los beneficiarios, basados en la información recopilada durante el proyecto.
+                </>
+            ),
+            buttonText: "Evaluación de impacto",
+            buttonLink: "/evaluation/impact",
+        },
+    ];
 
     return (
         <div className={styles.big_container}>
             <div className={styles.landing_container}>
                 <div className={`${styles.spacing} ${styles.landing}`}>
                     <div className={`${styles.spacing} ${styles.text} ${styles.title}`}>
-                        Plataforma de monitoreo, evaluación, aprendizaje y evaluación de impacto del proyecto Colombia
-                        Agroalimentaria Sostenible
+                        Plataforma de monitoreo, evaluación, aprendizaje y evaluación de impacto del proyecto Colombia Agroalimentaria Sostenible
                     </div>
                     <div className={`${styles.spacing} ${styles.text}`}>
-                        La Plataforma de Monitoreo y Evaluación es una herramienta diseñada para recopilar, gestionar y
-                        analizar indicadores clave del proyecto de manera eficiente y segura.
-                        <br/>
-                        Este sistema ofrece tableros interactivos que resumen el avance de los indicadores,
-                        proporcionando información en tiempo real para facilitar la toma de decisiones y mejorar la
-                        implementación del proyecto.
+                        La Plataforma de Monitoreo y Evaluación es una herramienta diseñada para recopilar, gestionar y analizar indicadores clave del proyecto de manera eficiente y segura.
+                        <br />
+                        Este sistema ofrece tableros interactivos que resumen el avance de los indicadores, proporcionando información en tiempo real para facilitar la toma de decisiones y mejorar la implementación del proyecto.
                     </div>
                     <div>
                         <button
@@ -36,54 +60,29 @@ export default function Home() {
                     </div>
                 </div>
             </div>
-            <div id={"information_section"} className={styles.second_container}>
-                <div className={`${styles.spacing} ${styles.second_title}`}>
-                    Resultados esperados:
-                </div>
+            <div id="information_section" className={styles.second_container}>
+                <div className={`${styles.spacing} ${styles.second_title}`}>Resultados esperados:</div>
                 <div className={`${styles.spacing} ${styles.card_container}`}>
-                    <div className={`${styles.spacing} ${styles.card}`}>
-                        <div className={`${styles.shadow} ${styles.chart_icon}`}>
-                            <img src="/chart_icon.png" alt="alliance-logo"/>
+                    {cards.map((card, index) => (
+                        <div key={index} className={`${styles.spacing} ${styles.card}`}>
+                            <div className={`${styles.shadow} ${styles.chart_icon}`}>
+                                <img src={card.icon} alt={card.alt} />
+                            </div>
+                            <div className={`${styles.spacing} ${styles.text}`}>{card.text}</div>
+                            <button
+                                className={`${styles.shadow} ${styles.spacing} ${styles.button} ${styles.card_button}`}
+                                onClick={() => (window.location.href = card.buttonLink)}
+                            >
+                                {card.buttonText}
+                            </button>
                         </div>
-                        <div className={`${styles.spacing} ${styles.text}`}>
-                            Lorem ipsum odor amet, consectetuer adipiscing elit. Urna at sit gravida fames tristique
-                            duis
-                            dictumst tempor malesuada. Mattis sociosqu ac libero erat, lobortis laoreet mus dictum.
-                            Hac
-                            gravida malesuada per purus; nisl sed. Metus venenatis sodales feugiat lectus libero
-                            potenti
-                            feugiat nullam.
-                        </div>
-                        <button
-                            className={`${styles.shadow} ${styles.spacing}  ${styles.button} ${styles.card_button}`}>
-                            Monitoreo
-                        </button>
-                    </div>
-                    <div className={`${styles.spacing} ${styles.card}`}>
-                        <div className={`${styles.shadow} ${styles.chart_icon}`}>
-                            <img src="/list_icon.png" alt="alliance-logo"/>
-                        </div>
-                        <div className={`${styles.spacing} ${styles.text}`}>
-                            Lorem ipsum odor amet, consectetuer adipiscing elit. Urna at sit gravida fames tristique
-                            duis
-                            dictumst tempor malesuada. Mattis sociosqu ac libero erat, lobortis laoreet mus dictum.
-                            Hac
-                            gravida malesuada per purus; nisl sed. Metus venenatis sodales feugiat lectus libero
-                            potenti
-                            feugiat nullam.
-                        </div>
-                        <button className={`${styles.shadow} ${styles.spacing} ${styles.button} ${styles.card_button}`}>
-                            Evaluación de impacto
-                        </button>
-                    </div>
+                    ))}
                 </div>
             </div>
             <div className={styles.second_container}>
-                <div className={styles.second_title}>
-                Socios clave
-                </div>
+                <div className={styles.second_title}>Socios clave</div>
                 <div className={styles.img_container}>
-                    <img src="/LOGOS%20-%203%20LÍNEAS.png" alt="alliance-logo"/>
+                    <img src="/LOGOS%20-%203%20LÍNEAS.png" alt="alliance-logo" />
                 </div>
             </div>
         </div>
